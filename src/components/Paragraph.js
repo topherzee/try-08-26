4247e2353b05c179bc9acc9911aa7d3b3aab03db
czimmermann <christopher.zimmermann@magnolia-cms.com>
2021-09-02 11:16:36 +0200

    fix name 'where' to paragraph dialog

--- src/components/Paragraph.js
+++ src/components/Paragraph.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
-const Paragraph = props => <p className="Paragraph" dangerouslySetInnerHTML={{ __html: props.richText }} />;
+const Paragraph = props => 
+
+    <>
+        <p className="Paragraph" dangerouslySetInnerHTML={{ __html: props.richText }} />
+        <br/>
+        <div>{props.where}</div>
+    </>;
 
 export default Paragraph;
